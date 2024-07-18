0184cdf21cb437b0bfeeb34f01791cbc431220dc
elburhan <burhanbobe@icloud.com>
2024-07-18 12:43:39 +0100

    Creating sidebardata for redux

---
+++ Frontend/kasuwandauri_inventory/src/redux/reducer/sidebardata.js
@@ -0,0 +1,33 @@
+import {createAsyncThunk, createSlice,crreateAsyncThunk} from '@reduxjs/toolkit';
+import axios from axios
+
+export const fetchSidebar=createAsyncThunk('data/fetchSidebar',async()=>{
+    const response=await axios.get('http://localhost:8000/api/sidebar/');
+    return response.data.data;
+    
+});
+
+const sidebarSlice=createSlice({
+    name:'data',
+    initialState:{
+        items:[],
+        status:'idle',
+        error:null
+    },
+    reducers:{
+
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(fetchSidebar.pending,(state,action)=>{
+            state.status='success';
+            state.items=action.payload;
+        })
+        addCase(fetchSidebar.rejected,(state,action)=>{
+            state.status='failed';
+            state.error=action.error.message;
+        })
+
+    }
+})
+
+export default sidebarSlice.reducer;
